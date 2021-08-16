f21da3bdbf1b61d0351657f0f7d5937582f0856c
99003688 <78859930+99003688@users.noreply.github.com>
2021-08-16 19:08:03 +0530

    Update main.go
    updated Ncr folder
    changes in main.go

--- golangTraining/Ncr/main.go
+++ golangTraining/Ncr/main.go
@@ -1,9 +1,27 @@
 package main
 
-import(
+import (
 	"fmt"
 )
 
-func main(){
-	fmt.Println("enter the series")
+func fact(n int) int {
+	var res int = 1
+	for i := 2; i <= n; i++ {
+		res = res * i
+	}
+	return res
+
+}
+
+func nCr(n int, r int) int {
+	return fact(n) / (fact(r) * fact(n-r))
+}
+
+func main() {
+	var n, r int
+	fmt.Println("Enter the value of n and r")
+	fmt.Scanln(&n)
+	fmt.Scanln(&r)
+	fmt.Println(nCr(n, r))
+
 }
